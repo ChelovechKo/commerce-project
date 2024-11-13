eb830b724a0aab5634ea4584df1d949fde68f2b7
zholudeva.e <ekaterinaadv@gmail.com>
2024-11-13 15:28:20 +0200

    fix sign_up

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.urls import reverse
